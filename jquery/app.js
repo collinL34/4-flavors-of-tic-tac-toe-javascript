00fc49d831790ef3dae0d6eadf5637639f9dece2
collinl34 <collinlshelby@gmail.com>
2017-01-14 13:47:51 -0800

    :art: styling add to index page along with bootstrap link

--- jquery/app.js
+++ jquery/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $(function() { $('.square').off('click') });
+    // $(function() { $('.square').off('click') });
 
     $('.replay').hide();
     var plays = [];
