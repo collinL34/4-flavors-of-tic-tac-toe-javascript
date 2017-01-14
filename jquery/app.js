6ca0dd367d3b475265ecea00dace293be395d31b
collinl34 <collinlshelby@gmail.com>
2017-01-14 13:27:53 -0800

    :sparkles: subfolders added for different flavors of game along with corresponding files

---
+++ jquery/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+
+    $(function() { $('.square').off('click') });
+
+    $('.replay').hide();
+    var plays = [];
+
+    $('.ready').on('click', function(event) {
+        $(this).hide();
+        $('.replay').show();
+        alert('Great lets play!');
+    });
+
+    $('.square').on('click', function(event) {
+        var square = event.target;
+        if (plays[plays.length - 1] !== 'X' || plays[plays.length - 1] === undefined) {
+            square.append('X');
+            plays.push('X');
+        } else {
+            square.append('O');
+            plays.push('O');
+        };
+        $(this).off('click');
+    });
+
+    $('.replay').on('click', function() {
+        location.reload();
+    });
+
+});
