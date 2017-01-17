300682067f05391787d42e8597a875443db60d25
collinl34 <collinlshelby@gmail.com>
2017-01-16 20:24:47 -0800

    :sparkles: angular controller and app create and implemented on index

---
+++ angularjs/angular-app.js
@@ -0,0 +1,11 @@
+const app = angular.module('TicTacToe', []);
+
+app.controller('MainCtrl', ['$scope', function($scope) {
+    $scope.board = [undefined,undefined,undefined,
+                    undefined, undefined, undefined,
+                    undefined, undefined, undefined
+    ],
+    $scope.click = function() {
+      
+    };
+}])
