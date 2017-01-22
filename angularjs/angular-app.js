ee1debd4916b08819773f0375766044916e4dff1
collinl34 <collinlshelby@gmail.com>
2017-01-22 12:45:14 -0800

    :sparkles: the winner is being declared but before the piece is placed

--- angularjs/angular-app.js
+++ angularjs/angular-app.js
@@ -8,18 +8,50 @@ app.controller('MainCtrl', function($scope) {
         undefined, undefined, undefined,
         undefined, undefined, undefined
     ];
-    
+
+    function calculateWinner(playerBoardIndexes, playerPiece) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (playerBoardIndexes[a] === playerPiece &&
+                playerBoardIndexes[b] === playerPiece && playerBoardIndexes[c] === playerPiece) {
+                return true;
+            };
+        };
+        return false;
+    };
+
     $scope.placePiece = function(clickedIndex) {
 
-      if ( playerX ) {
-        $scope.board[clickedIndex] = 'X';
-        playerX = false;
-        console.log(playerX);
-      } else {
-        $scope.board[clickedIndex] = 'O';
-        playerX = true;
-      };
+        if (playerX) {
+            $scope.board[clickedIndex] = 'X';
+            playerX = false;
 
+            if (calculateWinner($scope.board, 'X')) {
+                alert('player one wins!');
+            } else if (board.length === 9) {
+                alert('no winner');
+            };
+
+        } else {
+            $scope.board[clickedIndex] = 'O';
+            playerX = true;
+
+            if (calculateWinner($scope.board, 'O')) {
+                alert('player two wins!');
+            } else if (board.length === 9) {
+                alert('no winner');
+            };
+        };
     };
 
 });
