68a619656eb76247da530ef55dc452befc0b0bc4
collinl34 <collinlshelby@gmail.com>
2017-02-13 23:36:43 -0800

    material design for bootstrap add to site along with angular skeleton add

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
 // mongoose.connect();
-app.use(bodyParser.json()); 
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(methodOverride('X-HTTP-Method-Override')); 
-app.use(express.static(__dirname + '/public')); 
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080);
 
 console.log('App listening on port 8080');
 
-app.get('*', function(req,res) {
-  res.sendfile('index.html');
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
 });
